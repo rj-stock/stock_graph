70de60f6a44164aa055a909ad86360b65a530903
rj-stock <rongji_h@hotmail.com>
2023-09-02 18:50:00 +0800

    Polishing - x,y axis and cross pointer

--- static/kline.js
+++ static/kline.js
@@ -1,13 +1,13 @@
 import * as echarts from "https://cdn.jsdelivr.net/npm/echarts@5.4.3/dist/echarts.esm.min.js"
 import { getKData } from "./get_stock_data.js?ts=1"
 
-export default async function init(id = "main", code, { period } = {}) {
+export default async function init({ el, code, period } = {}) {
   const isDarkTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
   // get stock k data
   const stock = await getKData(code, period)
 
   // create the echarts instance
-  const chart = echarts.init(document.getElementById(id))
+  const chart = echarts.init(document.getElementById(el))
 
   // draw the chart
   chart.setOption(createChartOption(stock, isDarkTheme))
@@ -26,15 +26,57 @@ function shortenAmo(a) {
   else return a.toFixed(0) + "元"
 }
 
+function themeColors(isDarkTheme) {
+  return isDarkTheme
+    ? {
+      mainBg: "#000000",
+      mainBorder: "#4C2121",
+      // k 线
+      k: { upFill: "transparent", upBorder: "#FF3D3D", downFill: "#10CC55", downBorder: "#10CC55", doji: "#c23531" },
+      kTip: {
+        bg: "rgba(0, 0, 0, 0.8)",
+        border: "#144567",
+      },
+      // 均线
+      ma: {
+        opacity: 0.5,
+        0: "#FF3D3D",
+        5: "#00C6D1",
+        10: "#5BE58E",
+        20: "#6E79EF",
+        30: "#FF9A75",
+        60: "#19A0FF",
+        125: "#3948EF",
+        250: "#FF7575",
+      },
+      yAxis: {
+        name: "#EEEFF0", // 轴名
+        label: "#D03232", // 刻度值
+      },
+      cross: {
+        bg: "#5B387F",
+        line: "#ABADB2",
+        label: "#ABADB2",
+      },
+    }
+    : { mainBg: "#ffffff", mainBorder: "#4C2121" }
+}
+
+// dashed | solid | dotted
+const crossLineType = "solid"
+
 function createChartOption(stock, isDarkTheme) {
-  //stock.data = stock.data.slice(stock.data.length - 100) // test
+  const colors = themeColors(isDarkTheme)
+  stock.data = stock.data.slice(stock.data.length - 50) // test
   const len = stock.data.length
   // yesterday close: first K take open price, otherwise take pre close price
   const preC = len > 1 ? stock.data[len - 2].c : (len ? stock.data[0].o : undefined)
   const zhangFu = preC ? (stock.data[len - 1].c - preC) / preC * 100 : undefined
   // const up = len ? stock.data[len - 1].c >= stock.data[len - 1].o : undefined
-  const delta = 0 // 0.01
+  const delta = 0.01
+  const maRawData = stock.data.map(({ c }) => c)
   return {
+    backgroundColor: colors.mainBg,
     title: {
       left: "center",
       text: stock.code + (stock.name ? ` ${stock.name}` : ""),
@@ -46,43 +88,179 @@ function createChartOption(stock, isDarkTheme) {
         fontSize: 14,
       },
     },
+    axisPointer: {
+      link: { xAxisIndex: "all" },
+      label: {
+        backgroundColor: colors.cross.bg,
+        color: colors.cross.label,
+      },
+    },
     tooltip: {
-      backgroundColor: isDarkTheme ? "rgba(0, 0, 0, 0.8)" : "rgba(255, 255, 255, 0.8)",
-      formatter: (param) => tooltipFormatter(param, stock.data),
+      trigger: "axis",
+      axisPointer: {
+        type: "cross",
+        crossStyle: { color: colors.cross.line, type: crossLineType, width: 1 },
+        lineStyle: { color: colors.cross.line, type: crossLineType, width: 1 },
+      },
+      backgroundColor: colors.kTip.bg,
+      borderColor: colors.kTip.border,
+      position: function (pos, params, el, elRect, size) {
+        const obj = { top: 60 }
+        // 中间分割模式
+        obj[["left", "right"][+(pos[0] < size.viewSize[0] / 2)]] = "55em"
+        // TODO 保持固定直到鼠标进入提示框就反向定位
+        return obj
+      },
+      extraCssText: "width: auto",
+      valueFormatter: (v) => typeof v === "number" ? v.toFixed(2) : v,
+      // {a} 系列名
+      // {b} 数据名(x轴值)
+      // {c} 数据值 `[索引号, o, c, l, h, v, a]`
+      // formatter: "{a}、{b}、{c}、{d}、{e}",
+      formatter: (params) => tooltipFormatter(params[0], stock.data),
+    },
+    // 坐标轴组成的矩形的控制：left,right,top,bottom,width,height
+    grid: {
+      show: true,
+      containLabel: true,
+      borderColor: colors.mainBorder,
+      borderWidth: 2,
+      top: 60,
+      bottom: 0,
+      left: "25em",
+      right: "25em",
     },
     xAxis: {
+      show: false,
       // ["2017-10-24", "2017-10-25", "2017-10-26", "2017-10-27"],
       data: stock.data.map(({ t }) => t),
       axisLabel: {
         formatter: (date) => `${parseInt(date.substring(5, 7))}/${parseInt(date.substring(8))}`,
         align: "right",
       },
-      axisTick: {
-        alignWithLabel: true,
+      // axisTick: {
+      //   alignWithLabel: true,
+      // },
+      axisPointer: {
+        label: {
+          show: true,
+          formatter: ({ value }) => `${value} ${weekCn[new Date(value).getDay()]}`,
+        },
       },
     },
-    yAxis: {
-      type: "value",
-      min: Math.min(...stock.data.map(({ l }) => l)) - delta,
-      max: Math.max(...stock.data.map(({ h }) => h)) + delta,
-      axisLabel: {
-        formatter: (value) => value.toFixed(2),
-        align: "right",
+    yAxis: [
+      {
+        type: "value",
+        name: `日线 ${stock.name}`,
+        nameLocation: "end",
+        nameTextStyle: { color: colors.yAxis.name },
+        scale: true, // 设为 true 不限制显示 0 轴，设置了 min、max 则无效
+        axisLine: { show: false }, // 轴线
+        axisTick: { show: false }, // 刻度
+        splitLine: { show: false }, // 横向分割线
+        // 刻度值
+        axisLabel: {
+          show: true,
+          showMinLabel: false,
+          showMaxLabel: false,
+          color: colors.yAxis.label,
+          formatter: (value) => value.toFixed(2),
+          align: "right",
+        },
+        boundaryGap: false,
+        min: Math.min(...stock.data.map(({ l }) => l)) - delta,
+        max: Math.max(...stock.data.map(({ h }) => h)) + delta,
+        inside: false,
+        axisPointer: {
+          label: {
+            show: true,
+            precision: 2,
+          },
+        },
       },
-    },
+      {
+        type: "value",
+        // name: `日线 ${stock.name}`,
+        // nameTextStyle: { color: colors.yAxis.name },
+        scale: true, // 设为 true 不限制显示 0 轴，设置了 min、max 则无效
+        axisLine: { show: false }, // 轴线
+        axisTick: { show: false }, // 刻度
+        splitLine: { show: false }, // 横向分割线
+        // 刻度值
+        axisLabel: {
+          show: true,
+          showMinLabel: false,
+          showMaxLabel: false,
+          color: colors.yAxis.label,
+          formatter: (value) => value.toFixed(2),
+          align: "left",
+        },
+        boundaryGap: false,
+        min: Math.min(...stock.data.map(({ l }) => l)) - delta,
+        max: Math.max(...stock.data.map(({ h }) => h)) + delta,
+        inside: true,
+        axisPointer: {
+          label: {
+            show: true,
+            precision: 2,
+          },
+        },
+      },
+    ],
     series: [
       {
+        name: "日 K",
         type: "candlestick",
-        // object to array
         data: stock.data.map(({ o, c, l, h, v, a }) => [o, c, l, h, v, a]),
-        // data: [
-        //   [20, 34, 10, 38],
-        //   ...
-        // ],
+        //dimensions: ["date", "open", "close", "highest", "lowest"], // TODO
+        itemStyle: {
+          color: colors.k.upFill,
+          color0: colors.k.downFill,
+          borderColor: colors.k.upBorder,
+          borderColor0: colors.k.downBorder,
+          //borderColorDoji: colors.k.doji,
+        },
+        tooltip: {
+          // formatter: "{a}, {b}，{c}，{d}，{e}",
+        },
+      },
+      {
+        name: "MA5",
+        type: "line",
+        data: ma(5, maRawData),
+        silent: true,
+        animation: false,
+        smooth: false,
+        symbol: "none",
+        showSymbol: false,
+        lineStyle: {
+          opacity: colors.ma.opacity,
+          color: colors.ma[5],
+        },
+        tooltip: {
+          // formatter: "{a}, {b}，{c}，{d}，{e}",
+        },
+      },
+      {
+        name: "MA20",
+        type: "line",
+        data: ma(20, maRawData),
+        silent: true,
+        animation: false,
+        smooth: false,
+        // symbol: "none",
+        showSymbol: false,
+        lineStyle: {
+          opacity: colors.ma.opacity,
+          color: colors.ma[20],
+        },
       },
     ],
   }
 }
+
+const weekCn = ["", "周一", "周二", "周三", "周四", "周五", "周六", "周日"]
+
 function tooltipFormatter(param, kdata) {
   // param is {name:string, data:[index, o, c, l, h, v, a]}
   const { name, data: [index, o] } = param
@@ -91,7 +269,10 @@ function tooltipFormatter(param, kdata) {
   // first K take open price, otherwise take pre close price
   const preC = index > 0 ? kdata[index - 1].c : o
   return `<div style="display:flex;flex-direction:column">
-       <div>${name}</div>
+       <div style="display:flex">
+         <div style="text-align:left;">${name}</div>
+         <div style="text-align:right;flex-grow:1;padding-left:.5em">${weekCn[new Date(name).getDay()]}</div>
+       </div>
        <div style="display:flex">
          <div style="width:3.5em">开盘</div>
          <div style="text-align:right;flex-grow:1;">${curK.o.toFixed(2)}</div>
@@ -107,6 +288,10 @@ function tooltipFormatter(param, kdata) {
        <div style="display:flex">
          <div style="width:3.5em">最高</div>
          <div style="text-align:right;flex-grow:1;">${curK.h.toFixed(2)}</div>
+       </div>
+       <div style="display:flex">
+         <div style="width:3.5em">涨跌</div>
+         <div style="text-align:right;flex-grow:1;">${(curK.c - preC).toFixed(2)}</div>
        </div>
        <div style="display:flex">
          <div style="width:3.5em">涨幅</div>
@@ -126,3 +311,16 @@ function tooltipFormatter(param, kdata) {
        </div>
      </div>`
 }
+
+/**
+ * Calculate ma data.
+ * Empty data use "-" instead.
+ * @param {number} n Day count
+ * @param {number[]} values Raw data
+ * @param {number} fractionDigits Number of digits after the decimal point, default 2
+ * @returns {string[]}
+ */
+const ma = (n, values, fractionDigits = 2) =>
+  values.map((_, i) =>
+    i < n - 1 ? "-" : (values.slice(i - n + 1, i + 1).reduce((a, b) => a + b, 0) / n).toFixed(fractionDigits)
+  )
